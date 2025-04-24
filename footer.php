159567be0af985e194c6901a6e6a9cbd07e90cdc
jordan84292 <gonzalezjordan61@gmail.com>
2025-04-24 08:12:51 -0600

    se modifica el hover del subtittle

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer class="footer-container">
+      <nav class="container">
+        <a href="https://facebook.com" target="_blank"><i class="bi bi-facebook"></i></a
+        ><a href="https://twiter.com" target="_blank"><i class="bi bi-twitter-x"></i></a
+        ><a href="https://github.com" target="_blank"><i class="bi bi-github"></i></a
+        ><a href="https://youtube.com" target="_blank"><i class="bi bi-youtube"></i></a
+        ><a href="https://instagram.com" target="_blank"><i class="bi bi-instagram"></i></a
+        ><a href="https://messenger.com" target="_blank"><i class="bi bi-messenger"></i></a
+        ><a href="https://whatsapp.com" target="_blank"><i class="bi bi-whatsapp"></i></a>
+      </nav>
+      <small>&copy; Sitio Realizado por Jordan Gonzalez en 2025</small>
+    </footer>
--- index.php
+++ index.php
@@ -4,10 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gym Assistant</title>
-    
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
+    />
+    <link rel="stylesheet" href="./CSS/header.css">
+    <link rel="stylesheet" href="./CSS/footer.css">
     <link rel="stylesheet" href="./CSS/banner.css">
     <link rel="stylesheet" href="./CSS/nosotros.css">
-    <link rel="stylesheet" href="./CSS/header.css">
     <link rel="stylesheet" href="./CSS/servicios.css">
     
 </head>
@@ -27,6 +31,9 @@
     ?>
 <?php
     require_once "servicios.php"
+    ?>
+<?php
+    require_once "footer.php"
     ?>
 
 
